1ea8cc28cf09e11ef57cc63c7a34e1ac1dc722d7
mortada9999 <mortadamomo55@gmail.com>
2026-08-12 21:53:13 +0300

    fix(style): change svg clipPath to mask to restore wavy feTurbulence effect on torn edges

--- src/components/TornEdge.tsx
+++ src/components/TornEdge.tsx
@@ -31,16 +31,17 @@ export default function TornEdge({ color, seed, isNotebook = false }: TornEdgePr
             <feDisplacementMap in="SourceGraphic" in2="noise" scale="28" xChannelSelector="R" yChannelSelector="G" />
           </filter>
           
-          <clipPath id={clipId}>
+          <mask id={clipId}>
             <path
               d="M2437.62 112.088L2381.62 112.306C1606.78 115.31 854.364 122.328 463.232 125.977L445.164 126.146C59.7545 129.74 52.1082 129.745 44.5684 129.745H-11.4316V17.7451H44.5684C51.4158 17.7451 58.157 17.7498 444.119 14.1504L462.188 13.9814C853.316 10.3328 1606.01 3.3112 2381.18 0.305664L2437.18 0.0888672L2437.62 112.088Z"
               filter={`url(#${filterId})`}
+              fill="white"
             />
-            <rect x="-12" y="46" width="2450" height="100" />
-          </clipPath>
+            <rect x="-12" y="46" width="2450" height="100" fill="white" />
+          </mask>
         </defs>
 
-        <g clipPath={`url(#${clipId})`}>
+        <g mask={`url(#${clipId})`}>
           {/* Base solid color */}
           <rect x="-12" y="-12" width="2450" height="160" fill={color} />
           
